765535b421e64a789b5790b0e71caee84d9de382
waitunsoemysol <waitunsoe.mysol@gmail.com>
2024-10-24 17:10:25 +0630

    feat: solve the file upload 7 tests

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
 class ShopController extends Controller
@@ -16,6 +17,8 @@ class ShopController extends Controller
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
 
+        $resizeImage = Image::make($request->file('photo'))->resize(500, 500);
+        $resizeImage->save(Storage::path('shops/resized-'.$filename));
         return 'Success';
     }
 }
